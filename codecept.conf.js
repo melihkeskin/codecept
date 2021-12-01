4b32aa35c1fac0866cdd89461954f155c5066260
Melih Keskin <melihkeskinn@gmail.com>
2021-12-01 11:42:09 +0100

    test

--- codecept.conf.js
+++ codecept.conf.js
@@ -7,9 +7,7 @@ exports.config = {
     },
     Puppeteer: {
       url: 'http://dev.crpn.cds.arondor.cloud/flower-docs-gui',
-      show: false,
-      windowSize: '1280x960',
-      executablePath: '/usr/bin/chromium-browser',
+      headless:true,  
       args: [
         '--no-sandbox',
         '--lang=fr-FR']
